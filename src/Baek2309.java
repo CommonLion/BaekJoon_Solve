516df02c389c729b963d2580c9921fb01b8bdf19
CommonLion <tndud9548@kgu.ac.kr>
2023-12-27 22:35:10 +0900

    23.12.27

--- src/Baek2309.java
+++ src/Baek2309.java
@@ -1,2 +1,49 @@
-public class Baek2309 {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Baek2309{
+    public static void main(String[] args) {
+        ArrayList<Integer> ori_list = new ArrayList<>();
+        ArrayList<Integer> list;
+        Scanner s = new Scanner(System.in);
+        for (int i = 0; i < 9; i++) {
+            ori_list.add(s.nextInt());
+        }
+        list = new ArrayList<>(ori_list);
+        int sum=0;
+
+        for (int i = 0; i < 9; i++) {
+            for(int j=i+1; j < 9; j++){
+                sum=0;
+                list.remove(i);
+                list.remove(j-1);
+                for(int a:list){
+                    sum+=a;
+                }
+                if(sum==100){
+                    Collections.sort(list);
+                    for(int a : list){
+                        System.out.println(a);
+                    }
+                    return;
+                }
+                list = new ArrayList<>(ori_list);
+            }
+
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
